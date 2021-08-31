91c39112a622d63939525881447a324d85195d6d
dzq <dzq@aldelo.com>
2021-08-31 14:02:52 +0800

    first commit

---
+++ DqData/Classes/DqLog.swift
@@ -0,0 +1,15 @@
+//
+//  DqLog.swift
+//  DqData
+//
+//  Created by dzq_mac on 2021/8/31.
+//
+
+import UIKit
+
+class DqLog: NSObject {
+    func logPrint(_ str: String)  {
+        print("DqLog----" + str)
+    }
+
+}
---
+++ Example/Pods/Target Support Files/DqData/DqData-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_DqData : NSObject
+@end
+@implementation PodsDummy_DqData
+@end
